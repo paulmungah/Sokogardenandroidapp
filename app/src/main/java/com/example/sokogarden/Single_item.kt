88c06d81ec6ea8155d7037af80a857a1b132bb37
brightlord001 <pablomungah@gmail.com>
2026-04-16 16:09:12 +0300

    add

--- app/src/main/java/com/example/sokogarden/Single_item.kt
+++ app/src/main/java/com/example/sokogarden/Single_item.kt
@@ -30,7 +30,7 @@ class ProductAdapter(private val productList: List<Product>) :
     //Access the Layout - Single Item
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.single_item, parent, false)
+            .inflate(R.layout.activity_single_item, parent, false)
         return ProductViewHolder(view)
     }
 
